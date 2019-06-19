f6a8fefd42f03a0a733b066a11c883801c87fbf9
Jeremiah Nofrada <jnofrada@thevaliantway.com>
2019-06-19 11:34:45 -0400

    dictionary implementation

--- MacRename.py
+++ MacRename.py
@@ -1,20 +1,24 @@
 import subprocess
 
 #Opens computer name text ile
-comp = open("comp_names.txt", "r")
+comp = open("mother_devices.csv", "r")
+
+#Creates dictionary for with current computer names and new computer names
+mdict = {
+    "Current Name": "New Name"
+}
+
+for line in comp:
+    if not line: break
+    field = line.split(",")
+    mdict.update({field[0]:field[1]})
 
 #Gathers current computer name
 current_name = subprocess.call(["scutil", "--get", "computerName"])
 
-#Finds current computer name in text file and compares to current computer name
-for line in comp:
-    if not line: break
-    field = line.split(",")
-    name1 = field[0]
-    name2 = field[1]
-
-    if (current_name == name1):
-        #Renames computer name, host name, and local host name
-        subprocess.call(["sudo", "scutil", "--set", "LocalHostName", name])
-        subprocess.call(["sudo", "scutil", "--set", "HostName", name])
-        subprocess.call(["sudo", "scutil", "--set", "ComputerName", name])
+#Finds current computer name in dictionary
+if str(current_name) in mdict:
+    #Renames computer name, host name, and local host name
+    subprocess.call(["sudo", "scutil", "--set", "LocalHostName", mdict[current_name]])
+    subprocess.call(["sudo", "scutil", "--set", "HostName", mdict[current_name]])
+    subprocess.call(["sudo", "scutil", "--set", "ComputerName", mdict[current_name]])
